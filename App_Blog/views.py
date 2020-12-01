69db260a318c95652b7178be9fdee0c41dc8f7f1
MdTanvirFoysal <tanvirfaisal54@gmail.com>
2020-12-01 23:19:00 +0600

    Blog Comments

--- App_Blog/views.py
+++ App_Blog/views.py
@@ -4,6 +4,7 @@ from App_Blog.models import Blog, Comment, Likes
 from django.urls import reverse, reverse_lazy
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+from App_Blog.forms import CommentForm
 import uuid
 
 # Create your views here.
@@ -27,3 +28,20 @@ class BlogList(ListView):
     context_object_name = 'blogs'
     model = Blog
     template_name = 'App_Blog/blog_list.html'
+
+
+@login_required
+def blog_details(request, slug):
+    blog = Blog.objects.get(slug=slug)
+    comment_form = CommentForm()
+
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.user = request.user
+            comment.blog = blog
+            comment.save()
+            return HttpResponseRedirect(reverse('App_Blog:blog_details', kwargs={'slug': slug}))
+
+    return render(request, 'App_Blog/blog_details.html', context={'blog': blog, 'comment_form': comment_form})
